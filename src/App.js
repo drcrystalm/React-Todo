8bef7d79705203a8a4831ee1782e871f7d907e97
Crystal Martin <drcrystalm@gmail.com>
2019-05-30 20:51:56 -0500

    made some todo tasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React from "react"
+
+const todos = [
+    {
+        task: "clean house",
+        id: 100,
+        completed: false,
+    },
+    {
+        task: "cook dinner",
+        id: 101,
+        completed: false,
+    },
+    {
+        task: "feed dogs",
+        id: 102,
+        completed: false,
+    },
+    {
+        task: "clean cat box",
+        id: 103,
+        completed: false,
+    },
+    {
+        task: "exercise",
+        id: 103,
+        completed: false,
+    },
+]
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+    constructor() {
+        super()
+        this.state = {
+            todos,
+        }
+    }
+    // you will need a place to store your state in this component.
+    // design `App` to be the parent component of your application.
+    // this component is going to take care of state, and any change handlers you need to work with your state
+    render() {
+        return (
+            <div>
+                <h2>One Awesome Todo List!</h2>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Todo = props => {
+    return (
+        <div>
+            <p>Future task here</p>
+        </div>
+    )
+}
+
+export default Todo
